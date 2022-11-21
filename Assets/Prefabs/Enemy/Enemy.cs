1ed7542f6bb53aaef0696a74cf7be60e1e45e550
thiagonemecek <tgnemecek@gmail.com>
2022-11-21 18:47:08 -0500

    Remove a star package

--- Assets/Prefabs/Enemy/Enemy.cs
+++ Assets/Prefabs/Enemy/Enemy.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using Pathfinding;
 using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
