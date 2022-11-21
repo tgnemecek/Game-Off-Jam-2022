389dcb94ff7e737c94a1c9c91b679008cebd547e
thiagonemecek <tgnemecek@gmail.com>
2022-11-20 19:31:51 -0500

    Add SpawnMap

--- Assets/Prefabs/EnemyManager/EnemyManager.cs
+++ Assets/Prefabs/EnemyManager/EnemyManager.cs
@@ -7,32 +7,41 @@ public class EnemyManager : MonoBehaviour
   [SerializeField]
   private Board _board;
   [SerializeField]
-  private Collider _spawnArea;
-
+  private SpawnMap _spawnMap;
   [SerializeField]
   private List<Enemy> _nextWave = new List<Enemy>();
   private List<Enemy> _currentWave = new List<Enemy>();
 
+  void Start()
+  {
+    _spawnMap = Instantiate(_spawnMap, _board.transform);
+  }
+
   public void SpawnNextWave()
   {
+    int spawnMapIndex = 0;
+    var spawnPoints = _spawnMap.SpawnPoints;
+
     foreach (var e in _nextWave)
     {
-      Enemy enemy = Instantiate(e, _spawnArea.transform.position, Quaternion.identity, _spawnArea.transform);
-      enemy.transform.localPosition = GetRandomPositionInSpawnArea();
-      enemy.transform.localRotation = Quaternion.Euler(90, 0, 0);
+      Enemy enemy = Instantiate(e, spawnPoints[spawnMapIndex].position, Quaternion.identity, transform);
+      // enemy.transform.localPosition = GetRandomPositionInSpawnArea();
+      // enemy.transform.localRotation = Quaternion.Euler(90, 0, 0);
 
-      int randomIndex = UnityEngine.Random.Range(0, _board.Cards.Count - 1);
-      var randomCard = _board.Cards[randomIndex];
+      if (spawnMapIndex + 1 == spawnPoints.Count)
+      {
+        spawnMapIndex = 0;
+      }
+      else spawnMapIndex += 1;
+
       _currentWave.Add(enemy);
     }
   }
 
+
   public bool AreEnemiesStillAlive() => _currentWave.Count > 0;
 
-  public void RegisterEnemyDeath(Enemy enemy)
-  {
-    _currentWave.Remove(enemy);
-  }
+  public void RegisterEnemyDeath(Enemy enemy) => _currentWave.Remove(enemy);
   public void DisableAllEnemies()
   {
     foreach (var enemy in _currentWave)
@@ -40,14 +49,14 @@ public class EnemyManager : MonoBehaviour
       enemy.enabled = false;
     }
   }
-  private Vector3 GetRandomPositionInSpawnArea()
-  {
-    Vector3 extents = _spawnArea.transform.InverseTransformVector(_spawnArea.bounds.extents);
+  // private Vector3 GetRandomPositionInSpawnArea()
+  // {
+  //   Vector3 extents = _spawnArea.transform.InverseTransformVector(_spawnArea.bounds.extents);
 
-    return new Vector3(
-        Random.Range(-extents.x, extents.x),
-        Random.Range(-extents.y, extents.y),
-        0f
-    );
-  }
+  //   return new Vector3(
+  //       Random.Range(-extents.x, extents.x),
+  //       Random.Range(-extents.y, extents.y),
+  //       0f
+  //   );
+  // }
 }
