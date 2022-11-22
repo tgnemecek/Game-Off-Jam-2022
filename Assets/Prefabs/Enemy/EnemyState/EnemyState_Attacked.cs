afed777d4b5d7c5bb92e1e74bc04f348ce6089ea
ryanarine <ryannarine97@gmail.com>
2022-11-21 22:26:06 -0500

    Add enemy attacked animation

--- Assets/Prefabs/Enemy/EnemyState/EnemyState_Attacked.cs
+++ Assets/Prefabs/Enemy/EnemyState/EnemyState_Attacked.cs
@@ -7,7 +7,29 @@ public class EnemyState_Attacked : EnemyState
   public EnemyState_Attacked(Enemy context, EnemyStateFactory factory) : base(context, factory) { }
 
 
-  public override void EnterState() { }
+  public override void EnterState()
+  {
+    _context.StartCoroutine(AttackedAnimation());
+  }
+
+  IEnumerator AttackedAnimation()
+  {
+    float time = 1f / _context.EnemyConfig.AttackSpeed;
+    Vector3 scaleVector = Vector3.one * _context.EnemyConfig.AttackedShrinkScale;
+
+    LTSeq rotateSequence = LeanTween.sequence();
+
+    rotateSequence.append(LeanTween.rotateY(_context.gameObject, -1f * _context.EnemyConfig.AttackedRotationMultiplier, time / 2f));
+    rotateSequence.append(LeanTween.rotateY(_context.gameObject, 2f * _context.EnemyConfig.AttackedRotationMultiplier, time / 2f));
+    rotateSequence.append(LeanTween.rotateY(_context.gameObject, -1.5f * _context.EnemyConfig.AttackedRotationMultiplier, time / 2f));
+    rotateSequence.append(LeanTween.rotateY(_context.gameObject, 0.5f * _context.EnemyConfig.AttackedRotationMultiplier, time / 2f));
+
+    LeanTween.scale(_context.gameObject, scaleVector, time).setOnComplete(() => LeanTween.scale(_context.gameObject, Vector3.one, time));
+    
+    yield return new WaitForSeconds(time);
+    SwitchState(_factory.InBattle());
+  }
+
   public override void UpdateState() { }
   public override void FixedUpdateState() { }
   public override void ExitState() { }
